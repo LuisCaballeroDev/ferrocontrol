cbe22ac8ce50d4b13db49ecfbc505636655c34a8
Luis Caballero <luiscaballero.dev@outlook.com>
2026-08-02 23:32:57 -0600

    Se crea la excepcion de dominio

---
+++ src/Services/Catalog/Catalog.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Domain.Exceptions
+{
+    public sealed class DomainException : Exception
+    {
+        public DomainException(string message) 
+            : base(message)
+        {
+        }
+    }
+}
