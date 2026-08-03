947e756b641407accb38e34f99aa99201b8f0b25
Luis Caballero <luiscaballero.dev@outlook.com>
2026-08-02 23:30:30 -0600

    Se crea entidad Categoria

---
+++ src/Services/Catalog/Catalog.Domain/Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.Domain.Entities
+{
+    public sealed class Category
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string? Description { get; private set; }
+        public bool IsActive { get; private set; }
+        public DateTime CreateAtUtc { get; private set; } 
+        public DateTime? UpdateAtUtc { get; private set; }
+
+        private Category() {
+            Name = string.Empty;
+        }
+
+        public Category(string name, string? description)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            IsActive = true;
+            CreateAtUtc = DateTime.UtcNow;
+        }
+
+    }
+}
